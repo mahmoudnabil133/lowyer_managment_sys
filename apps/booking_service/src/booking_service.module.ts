36131ca9d0e8e144f85185f24b6bf1cb56e46be4
mahmoudnabil133 <hodanabil155@gmail.com>
2026-05-03 20:43:41 +0300

    Add other config data

--- apps/booking_service/src/booking_service.module.ts
+++ apps/booking_service/src/booking_service.module.ts
@@ -1,10 +1,11 @@
 import { Module } from '@nestjs/common';
 import { BookingServiceController } from './booking_service.controller';
 import { BookingServiceService } from './booking_service.service';
+import { AppointmentController } from './appointment/appointment.controller';
 
 @Module({
   imports: [],
-  controllers: [BookingServiceController],
+  controllers: [BookingServiceController, AppointmentController],
   providers: [BookingServiceService],
 })
 export class BookingServiceModule {}
