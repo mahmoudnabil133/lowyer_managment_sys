75ce419e6c8018a8dabb8c6a774bc542998e4f4d
mahmoudnabil133 <hodanabil144@gmail.com>
2026-06-28 21:13:09 +0300

    refactor(auth): update profile.controller.ts

--- apps/auth_service/src/user/controllers/profile.controller.ts
+++ apps/auth_service/src/user/controllers/profile.controller.ts
@@ -10,18 +10,22 @@ import {
 import { AuthGuard } from '@nestjs/passport';
 import { UserService } from '../user.service';
 import { UpdateUserDto } from '../dto/updateUser.dto';
-import express from 'express';
 
 @Controller('profile')
 @UseGuards(AuthGuard('jwt'))
 export class ProfileController {
-  constructor(private readonly usersService: UserService) {}
+  constructor(private readonly usersService: UserService) { }
 
   // 🔹 GET MY PROFILE
   @Get()
   async getMyProfile(@Req() req: any) {
+    console.log("my user", req.user);
     const userId = req.user.userId;
     return this.usersService.findOne(userId);
+  }
+  @Get()
+  async test() {
+    return 'test';
   }
 
   // 🔹 UPDATE MY PROFILE
@@ -42,7 +46,7 @@ export class ProfileController {
   }
 
   @Post()
-  async changePassword(@Req() req: any, @Body() body: {oldPassword: string, newPassword: string}){
+  async changePassword(@Req() req: any, @Body() body: { oldPassword: string, newPassword: string }) {
     let userId = req.user.userId;
     return await this.usersService.changePassword(userId, body.oldPassword, body.newPassword);
   }
