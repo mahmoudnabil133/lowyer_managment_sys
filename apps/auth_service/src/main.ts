b25d48b42dbbafac9fadeaf5d76403e3c2101d73
mahmoudnabil133 <hodanabil144@gmail.com>
2026-06-28 21:13:09 +0300

    refactor(auth): update main.ts

--- apps/auth_service/src/main.ts
+++ apps/auth_service/src/main.ts
@@ -2,13 +2,14 @@ import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 import { ValidationPipe } from '@nestjs/common';
 import cookieParser from 'cookie-parser';
-import { CatchExceptionsFilter } from './common/global.filter';
+import { CatchExceptionsFilter } from '../../../libs/common/src/global/filters/global.filter';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
   app.useGlobalPipes(new ValidationPipe());
   app.setGlobalPrefix('api/v1/')
   app.use(cookieParser());
+  console.log(process.env.AUTH_PORT, 'AUTH_PORT');
   await app.listen(process.env.AUTH_PORT ?? 3000);
 }
 bootstrap();
