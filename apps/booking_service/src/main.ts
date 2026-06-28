68161cd4e7b97d0fcacaa6b41e0a61a0480eedd8
mahmoudnabil133 <hodanabil144@gmail.com>
2026-06-28 21:13:09 +0300

    refactor(booking): update main.ts

--- apps/booking_service/src/main.ts
+++ apps/booking_service/src/main.ts
@@ -1,8 +1,15 @@
 import { NestFactory } from '@nestjs/core';
 import { BookingServiceModule } from './booking_service.module';
+import { ValidationPipe } from '@nestjs/common';
 
 async function bootstrap() {
   const app = await NestFactory.create(BookingServiceModule);
+  app.useGlobalPipes(new ValidationPipe({
+    whitelist: true, // remove extra properties
+    // forbidNonWhitelisted: true, // throw error if extra properties are found
+    transform: true // transform payloads to DTOs
+  }));
+  app.setGlobalPrefix('api/v1');
   await app.listen(process.env.BOOKING_PORT ?? 3002);
 }
 bootstrap();
