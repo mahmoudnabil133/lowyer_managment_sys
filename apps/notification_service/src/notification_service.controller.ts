bfabb6b6756225c289bb45002c2bc8bdf695b246
mahmoudnabil133 <hodanabil144@gmail.com>
2026-06-28 21:13:09 +0300

    refactor(notification): update notification_service.controller.ts

--- apps/notification_service/src/notification_service.controller.ts
+++ apps/notification_service/src/notification_service.controller.ts
@@ -3,21 +3,42 @@ import { NotificationServiceService } from './notification_service.service';
 import { Ctx, EventPattern, Payload, RmqContext } from '@nestjs/microservices';
 import { RmqService } from '@app/rmq';
 
-@Controller()
+@Controller('notification')
 export class NotificationServiceController {
-  constructor(private readonly notificationServiceService: NotificationServiceService, private readonly rmqService: RmqService) {}
+  constructor(private readonly notificationServiceService: NotificationServiceService, private readonly rmqService: RmqService) { }
 
-  @Get()
+  @Get('hello')
   getHello(): string {
     return this.notificationServiceService.getHello();
   }
   @EventPattern('user_created')
   async handleUserCreated
-  (
-    @Payload() data: { userId: string, email: string },
-    @Ctx() context:RmqContext,
-  ) {
+    (
+      @Payload() data: { userId: string, email: string },
+      @Ctx() context: RmqContext,
+    ) {
     console.log('Received user_created event with data:', data);
+    this.rmqService.ack(context);
+  }
+
+
+  @EventPattern('appointment.booked')
+  async handleAppBooked
+    (
+      @Payload() data: any,
+      @Ctx() context: RmqContext,
+    ) {
+    console.log('Received appointment.booked event with data:', data);
+    this.rmqService.ack(context);
+  }
+
+  @EventPattern('appointment.cancelled')
+  async handleAppCancelled
+    (
+      @Payload() data: any,
+      @Ctx() context: RmqContext,
+    ) {
+    console.log('Received appointment.cancelled event with data:', data);
     this.rmqService.ack(context);
   }
 
