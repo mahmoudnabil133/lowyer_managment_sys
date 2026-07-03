481002b7dabdcacb22db3d3a2775db2517bd6d3a
mahmoudnabil133 <hodanabil144@gmail.com>
2026-07-03 17:41:47 +0300

    feat(booking): start microservice listener for payment events

--- apps/booking_service/src/main.ts
+++ apps/booking_service/src/main.ts
@@ -1,6 +1,7 @@
 import { NestFactory } from '@nestjs/core';
 import { BookingServiceModule } from './booking_service.module';
 import { ValidationPipe } from '@nestjs/common';
+import { RmqService } from '@app/rmq';
 
 async function bootstrap() {
   const app = await NestFactory.create(BookingServiceModule);
@@ -10,6 +11,10 @@ async function bootstrap() {
     transform: true // transform payloads to DTOs
   }));
   app.setGlobalPrefix('api/v1');
+  const rmqService = app.get(RmqService);
+  app.connectMicroservice(rmqService.getOptions('booking_queue'));
+  await app.startAllMicroservices();
+
   await app.listen(process.env.BOOKING_PORT ?? 3002);
 }
 bootstrap();
